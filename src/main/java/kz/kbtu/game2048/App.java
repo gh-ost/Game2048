4e216a608736079927917fd7c6487be39bd9bd39
Sanzhar <Sanzhar@Sanzhar-PC>
2014-10-22 23:54:39 +0600

    First working version

--- src/main/java/kz/kbtu/game2048/App.java
+++ src/main/java/kz/kbtu/game2048/App.java
@@ -1,13 +1,25 @@
 package kz.kbtu.game2048;
 
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+import kz.kbtu.game2048.model.GameTable;
+
 /**
- * Hello world!
  *
+ * @author Sanzhar
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        JFrame game = new JFrame();
+        game.setTitle("2048 Game");
+        game.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        game.setSize(340, 400);
+        game.setResizable(false);
+
+        game.add(new GameTable());
+
+        game.setLocationRelativeTo(null);
+        game.setVisible(true);
     }
 }
